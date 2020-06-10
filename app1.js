832ef597ebfbe6029e07dc6e0f6aa2ee87fe8eb7
aman22032007 <66054113+aman22032007@users.noreply.github.com>
2020-06-10 16:21:58 +0530

    k

--- app1.js
+++ app1.js
@@ -1,4 +1,4 @@
-var score,roundscore,activeplayerside,activeplayer,dice,a,gameplaying;
+var score,roundscore,activeplayerside,activeplayer,dice,a,diceprevious;
 score=[0,0];
 roundscore=0;
 activeplayer=0;
@@ -11,27 +11,41 @@ document.querySelector('#text0').textContent='0';
 document.querySelector('#text1').textContent='0';
 
 document.querySelector('.btn-roll').addEventListener('click',function(){
-    if(gameplaying){
+if(gameplaying){
 var dice=Math.floor(Math.random()*6)+1;
 var diceDOM=document.querySelector('#aman');
 diceDOM.style.display="block";
 diceDOM.src="dice-"+dice+".png";
-if(dice!==1){
+if(diceprevious===6&&dice===6){
+    score[activeplayer]=0;
+    document.getElementById("score-"+activeplayer).textContent="0";
+    document.getElementById('#score-0').style="left:22%";
+    nextplayer();
+}
+else if(dice!==1){
     roundscore+=dice;
     document.querySelector('#text'+activeplayer).textContent=roundscore;
 }
 else{
     nextplayer()
+}    
 }
-    }
-})
+diceprevious=dice;})
 document.querySelector('.btn-hold').addEventListener('click',function(){
+var input=document.querySelector("#winnerscore").value;
+var winnerscore;
+if(input){
+winnerscore=input
+}
+else{
+winnerscore=100;
+}
     if(gameplaying){
 score[activeplayer]+=roundscore;
 document.getElementById('score-'+activeplayer).textContent=score[activeplayer];
-if(score[activeplayer]>=100){
+if(score[activeplayer]>=winnerscore){
     document.querySelector("#player-"+activeplayer).textContent="WINNER!";
-    document.querySelector("#player-"+activeplayer).style="color:red;"
+    document.querySelector("#player-"+activeplayer).style="color:red;left:22%"
     document.querySelector('#aman').style.display="none";
     document.getElementById('left').classList.remove("active");
     document.getElementById('right').classList.remove("active");
@@ -44,7 +58,9 @@ if(score[0]>=10){
     document.querySelector('#score-0').style="left:11%";
 }
     }
-})
+    
+}
+)
 document.querySelector('.btn-new').addEventListener('click',function(){
     score=[0,0];
     roundscore=0;
@@ -74,4 +90,3 @@ function nextplayer(){
     document.getElementById('right').classList.toggle("active");
     document.querySelector('.dice').style.display="block"
 }
-  
